e71172be5c91cce690448ee464d221573b90322a
Tharushan Rathnaweera <thetriplet3@gmail.com>
2018-05-31 16:14:28 +0530

    Added a post message

--- jokes/joke.route.js
+++ jokes/joke.route.js
@@ -3,5 +3,6 @@ var router = express.Router()
 
 var JokeController = require('../jokes/joke.controller');
 router.get('/', JokeController.getDadJoke);
+router.post('/', JokeController.getDadJoke);
 
 module.exports = router;
