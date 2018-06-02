f662682496c09153d3449cbafceb54dafa0d6179
Tharushan Rathnaweera <thetriplet3@gmail.com>
2018-06-02 09:46:53 +0530

    removed get route

--- jokes/joke.route.js
+++ jokes/joke.route.js
@@ -4,7 +4,7 @@ var router = express.Router()
 var JokeController = require('../jokes/joke.controller');
 var JokeAgentController = require('../jokes/joke.agent.controller');
 
-router.get('/', JokeController.getDadJoke);
+//router.get('/', JokeController.getDadJoke);
 router.post('/agent/', JokeController.getDadJoke);
 
 module.exports = router;
